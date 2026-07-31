aa64adfd034ccfeddbf4fbb1b4eb8db0d6422c5d
ian254-byte <iankipro273@gmail.com>
2026-07-31 12:57:22 +0300

    DB  Models complete

---
+++ Backend/models/Task.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const taskSchema = new mongoose.Schema({
+    name: {type:String , required: true },
+    description: {type:String},
+    completed: {type: Boolean, default: false},
+
+    author: {type: mongoose.Schema.Types.ObjectId, ref:"User" ,required:true}
+
+
+})
