d15c5295557541a5eb8706f0b18f91cd2ac2f871
ian254-byte <iankipro273@gmail.com>
2026-09-08 10:59:04 +0300

    frontend initialized

--- Backend/models/Task.js
+++ Backend/models/Task.js
@@ -9,4 +9,4 @@ const taskSchema = new mongoose.Schema({
 
 
 })
-module.exports= mongoose.model('Task', taskSchema)
+module.exports = mongoose.model('Task', taskSchema)
--- Backend/models/User.js
+++ Backend/models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
     username: {type: String ,required:true},
     email: {type:String , unique: true, required:true},
     password: {type: String, required:true},
-    role: {type:String,enum:['developer', 'admin'], default:'developer'}
+    role: {type:String, enum:['developer', 'admin'], default:'developer'}
 });
 
 exports.module = mongoose.model('User', userSchema);
