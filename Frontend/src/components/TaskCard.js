f5e737501057c4f33bf541e53652ad2519011b10
ian254-byte <iankipro273@gmail.com>
2026-09-14 18:15:41 +0300

    Task Card imports done

---
+++ Frontend/src/components/TaskCard.js
@@ -0,0 +1,18 @@
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { TrashIcon, CheckCircleIcon } from "@heroicons/react/16/solid";
+import {Button} from "@/components/ui/button"
+
+export default function TaskCard ({onDelete, onToggle, task}) {
+   return(
+    
+   )
+
+}
