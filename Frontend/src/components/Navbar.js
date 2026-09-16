a77b3db8b645984fcc5bfe99e977e8b0db503834
ian254-byte <iankipro273@gmail.com>
2026-09-16 23:36:23 +0300

    Navbar initialized

---
+++ Frontend/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+import { Button } from "@/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
