5142593aea1e68fb119749c251dd57b9b32c8a2a
ian254-byte <iankipro273@gmail.com>
2026-09-17 10:14:10 +0300

    Utility function initialized

---
+++ Frontend/src/utils/auth.js
@@ -0,0 +1,25 @@
+//utility function
+
+export const decodeToken = (token) => {
+
+};
+
+export const getUserFromToken = () => {
+
+}
+
+export const getRole = () => {
+
+}
+
+export const isAdmin = () => {
+
+}
+
+export const isDeveloper = () => {
+
+}
+
+export const getUsername = () => {
+
+};
