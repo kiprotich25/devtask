45ebd2d4edc01b8916b5563070fac53cb6a7a27c
ian254-byte <iankipro273@gmail.com>
2026-09-17 10:43:22 +0300

    get user utility donne

--- Frontend/src/utils/auth.js
+++ Frontend/src/utils/auth.js
@@ -1,18 +1,33 @@
 //utility function
 
 export const decodeToken = (token) => {
+   try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    return payload;
+   } catch (error) {
+    console.error("Error decoding token :", error)
+    return null;
+   } 
 
 };
 
 export const getUserFromToken = () => {
-
-}
+    
+       const token = localStorage.getItem('token')
+       const user = decodeToken(token)
+       return user;
+   
+};
 
 export const getRole = () => {
+    const user = getUserFromToken();
+    return user? user.role : null
+
 
 }
 
 export const isAdmin = () => {
+    const 
 
 }
 
