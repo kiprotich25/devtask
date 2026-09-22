bcb7025aba260acb69a3ca3ad142c519632f139a
ian254-byte <iankipro273@gmail.com>
2026-09-22 12:54:52 +0300

    Constants added for signup

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -1,6 +1,28 @@
 import {useState} from "react"
 import {useNavigate} from "react-router-dom"
-import {Link} from "@/components/ui"
+import {Link} from "react-router-dom"
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import {API} from "../services/api"
+
+export default function Signup() {
+    const [username, setUserame] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+
+    const handleSignup = async()=> {
+
+    }
+
+
+
+
+    return(
+
+    )
+
+}
+  
