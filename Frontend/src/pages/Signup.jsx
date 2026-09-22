088f2f4e89909ddc89f9654e41e5ac1b808d7384
ian254-byte <iankipro273@gmail.com>
2026-09-22 12:41:14 +0300

    Added Signup pages - ongoing devt

---
+++ Frontend/src/pages/Signup.jsx
@@ -0,0 +1,6 @@
+import {useState} from "react"
+import {useNavigate} from "react-router-dom"
+import {Link} from "@/components/ui"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
