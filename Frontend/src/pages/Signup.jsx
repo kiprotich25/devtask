4493c26407abef490fd8f89405dfe7be299dea68
ian254-byte <iankipro273@gmail.com>
2026-09-23 17:36:59 +0300

    Main div in signup donne

--- Frontend/src/pages/Signup.jsx
+++ Frontend/src/pages/Signup.jsx
@@ -14,13 +14,50 @@ export default function Signup() {
     const navigate = useNavigate();
 
     const handleSignup = async()=> {
+     if (!username || !email || !password) return alert ("All fields are required")
+     setLoading(true);
+
+     try {
+        const res = await API.post("/auth/signup" , {username, email, password});
+        localStorage.setItem("token", res.data.token);
+        navigate("/dashboard")
+        
+     } catch (err) {
+        alert (err.response?.data?.message || "Sign-up failed")
+        
+        
+     } finally {
+       setLoading(false)
+     }
+  
 
     }
 
 
 
-
     return(
+        <div className="flex min-h-screen  justify-center items-center bg-gray-100 dark:bg-zinc-900 px-4">
+            <Card>
+              <CardHeader>
+                <CardTitle></CardTitle>
+              </CardHeader>
+              
+              <CardContent>
+                <Input/>
+                <Input/>
+                <Input/>
+              </CardContent>
+
+              <CardFooter>
+                <Button></Button>
+              </CardFooter>
+
+              <p>
+                <Link>
+                </Link>
+              </p>
+            </Card>
+        </div>
 
     )
 
